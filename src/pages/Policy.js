adb6871907b61fc580480be90c5c1845d02ae8c8
Joseph Archer <viewtiful.kja@gmail.com>
2022-11-21 00:01:05 -0500

    dumb mistake

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -7,7 +7,7 @@ function Policy() {
         <>
             <a id='phone' className='m-3' href='tel:5862586841'>
                 <div className='flex items-center justify-end  w-content'>
-                    <img src={phoneIco} />
+                    {/* <img src={phoneIco} /> */}
                     <p>586-258-6841</p>
                 </div>
             </a>
